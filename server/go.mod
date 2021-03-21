8b42372ea70fff2a97ca0f9f706c9185c19a361f
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-21 19:27:41 +0900

    goでdb接続、crud作成

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,9 @@
 module server
 
 go 1.16
+
+require (
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+	gorm.io/gorm v1.21.4 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,15 +1,120 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
-func hello(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w,"Hello World!")
+type Todo struct {
+	ID      int
+	Text    string
+	Created time.Time
 }
 
-func main()  {
-	http.HandleFunc("/",hello)
-	http.ListenAndServe(":8080",nil)
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "host=db user=root password=root dbname=next_go sslmode=disable")
+	checkErr(err)
+}
+
+func main() {
+	defer db.Close()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorizatio")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	var f Todo
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&f)
+		checkErr(err)
+	}
+
+	switch r.URL.Path {
+	case "/create":
+		create(f.Text)
+		todo := read()
+		js, err := json.Marshal(todo)
+		checkErr(err)
+		w.Write(js)
+	case "/read":
+		todo := read()
+		js, err := json.Marshal(todo)
+		checkErr(err)
+		w.Write(js)
+	case "/update":
+		update(f.ID, f.Text)
+		todo := read()
+		js, err := json.Marshal(todo)
+		checkErr(err)
+		w.Write(js)
+	case "/delete":
+		delete(f.ID)
+		todo := read()
+		js, err := json.Marshal(todo)
+		checkErr(err)
+		w.Write(js)
+	default:
+		fmt.Println("Noting")
+	}
+}
+
+func create(text string) {
+	stmt, err := db.Prepare("INSERT INTO todos (text,created) VALUES ($1,$2) RETURNING id")
+	checkErr(err)
+	_, err = stmt.Exec(text, time.Now())
+	checkErr(err)
+}
+
+func read() []Todo {
+	rows, err := db.Query("SELECT * FROM todos")
+	checkErr(err)
+	var todo []Todo
+	for rows.Next() {
+		var id int
+		var text string
+		var created time.Time
+		err = rows.Scan(&id, &text, &created)
+		checkErr(err)
+		todo = append(todo, Todo{id, text, created})
+	}
+	return todo
+}
+
+func update(id int, text string) {
+	stmt, err := db.Prepare("UPDATE todos SET text=$1 WHERE id=$2")
+	checkErr(err)
+	_, err = stmt.Exec(text, id)
+	checkErr(err)
+}
+
+func delete(id int) {
+	stmt, err := db.Prepare("DELETE FROM todos WHERE id=$1")
+	checkErr(err)
+	_, err = stmt.Exec(id)
+	checkErr(err)
+}
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
